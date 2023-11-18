68b2e34cfc4b22116e8878234a6213d69594b51d
adarshvardhansingh12 <147041273+adarshvardhansingh12@users.noreply.github.com>
2023-11-18 11:59:48 +0530

    Create condition.c

---
+++ comdition.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int x;
+  printf("enetr the number :");
+  scanf("%d",&x);
+  (x==10||x==100)?printf("i am learning c programing"):printf("i am not learning c programing ");
+  return 0;
+}
