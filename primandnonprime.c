8e381fe970d48c7bb977e4fb9625f5cb36d301af
adarshvardhansingh12 <147041273+adarshvardhansingh12@users.noreply.github.com>
2023-11-20 12:01:36 +0530

    Create primandnonprime.c

---
+++ primandnonprime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,arr[n],i,p=0,np=0;
+    printf("enter the size of array :");
+    scanf("%d",&n);
+    printf("Enter the arr element : ");
+
+    for (i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+
+         for(int j=0;j<n;j++){
+        }
+    }
+    
+}
