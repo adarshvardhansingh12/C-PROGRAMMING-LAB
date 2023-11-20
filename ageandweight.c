89bb13a76aed7d1c5241989f89c869cc929d56da
adarshvardhansingh12 <147041273+adarshvardhansingh12@users.noreply.github.com>
2023-11-20 11:50:43 +0530

    Create ageandweight.c

---
+++ ageandweight.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i ,x, age , weight;
+    printf("Enter the age and weigth of 5 students \n");
+    printf("age \t weight\n");
+
+    for(i=1;i<=5;i++){
+        scanf("%d \t %d",&age,weight);
+      
+       
+    
+
+    }  
+    if(age<25&&weight<50){
+        x++;
+    }
+    printf("%d",x);
+
+     //printf("%d\n",age);
+    // printf("%d\n",weight);
+}
