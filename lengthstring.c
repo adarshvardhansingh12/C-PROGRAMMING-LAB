7c27106665d425822e8ace41d02fe8a1c1dbdb14
adarshvardhansingh12 <147041273+adarshvardhansingh12@users.noreply.github.com>
2023-11-23 12:02:42 +0530

    Create lengthstring.c

---
+++ lengthstring.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    char a[100];
+    printf("enter the string ");
+    scanf("%[^\n]s",a);
+    int l=0;
+    for(int i=0;;i++){
+        if (a[i]=='\0')
+        break;
+    
+    else
+    {
+        l++;
+        }
+    }
+    printf("The lenght is =%d",l);
+}
